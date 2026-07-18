61368711480f9c062efb2f2e6538b8f57a6365a4
Prasanna <prasannavenkatesh.m@arisecapital.in>
2026-07-18 18:25:25 +0530

    Feat : Bottom Navigation bar added

---
+++ lib/app/widgets/bottom_nav.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class HomeBottomNav extends StatelessWidget {
+  final int currentIndex;
+  final ValueChanged<int>? onTap;
+
+  const HomeBottomNav({super.key, this.currentIndex = 0, this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: onTap,
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: Colors.black,
+      selectedItemColor: Colors.yellow,
+      unselectedItemColor: Colors.grey,
+      selectedFontSize: 12,
+      unselectedFontSize: 12,
+      elevation: 12,
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home_rounded), label: "Home"),
+
+        BottomNavigationBarItem(
+          icon: Icon(Icons.campaign_rounded), // or Icons.report_problem_rounded
+          label: "Grievances",
+        ),
+
+        BottomNavigationBarItem(
+          icon: Icon(Icons.support_agent_rounded),
+          label: "Helpdesk",
+        ),
+        // BottomNavigationBarItem(
+        //   icon: Icon(Icons.person_outline),
+        //   label: "Profile",
+        // ),
+      ],
+    );
+  }
+}
