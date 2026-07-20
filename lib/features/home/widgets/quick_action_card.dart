4787ff0eb16566677e29743f6ea661b8d222671c
Prasanna <prasannavenkatesh.m@arisecapital.in>
2026-07-20 19:24:50 +0530

    Feat : Quick Action card

--- lib/features/home/widgets/quick_action_card.dart
+++ lib/features/home/widgets/quick_action_card.dart
@@ -1,40 +1,67 @@
 import 'package:flutter/material.dart';
 
-import '../../../core/constants/app_colors.dart';
-
 class QuickActionCard extends StatelessWidget {
-  final IconData icon;
+  final String  icon;
   final String title;
+  final VoidCallback? onTap;
 
-  const QuickActionCard({super.key, required this.icon, required this.title});
+  const QuickActionCard({
+    super.key,
+    required this.icon,
+    required this.title,
+    this.onTap,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      elevation: 4,
-      shadowColor: Colors.black12,
-      borderRadius: BorderRadius.circular(18),
-      child: Container(
-        height: 115,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(18),
-          gradient: const LinearGradient(
-            colors: [Color(0xffFFD54F), Color(0xffFFC107)],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-          ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+    return GestureDetector(
+      onTap: onTap,
+      child: SizedBox(
+        height: 88,
+        child: Stack(
           children: [
-            Icon(icon, size: 34, color: AppColors.primary),
+            // Offset golden layer
+            Positioned(
+              left: 3,
+              top: 3,
+              right: 0,
+              bottom: 0,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: const Color(0xFFD4A017),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
 
-            const SizedBox(height: 12),
-
-            Text(
-              title,
-              textAlign: TextAlign.center,
-              style: const TextStyle(fontWeight: FontWeight.w700, fontSize: 13),
+            // Main white card
+            Positioned(
+              left: 0,
+              top: 0,
+              right: 3,
+              bottom: 3,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                  border: Border.all(color: Colors.black87, width: 1.5),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(icon, style: const TextStyle(fontSize: 28)),
+                    const SizedBox(height: 8),
+                    Text(
+                      title,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 13,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
