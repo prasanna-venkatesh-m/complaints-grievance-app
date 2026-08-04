53eab3e62058d1008c7f7da7e775da42e8e03f4f
Prasanna <prasannavenkatesh.m@arisecapital.in>
2026-08-04 10:15:54 +0530

    Feat : Latest updates api in Home controller

--- lib/features/home/home_controller.dart
+++ lib/features/home/home_controller.dart
@@ -1,14 +1,25 @@
+import 'package:flutter/foundation.dart';
 import 'home_model.dart';
 import 'home_repository.dart';
 
-class HomeController {
+class HomeController extends ChangeNotifier {
   final HomeRepository repository;
 
   HomeController(this.repository);
 
-  List<DashboardStat> get stats =>
-      repository.dashboardStats();
+  List<DashboardStat> get stats => repository.dashboardStats();
 
-  List<HomeUpdate> get updates =>
-      repository.latestUpdates();
+  List<LatestUpdate> updates = [];
+
+  bool isLoadingUpdates = false;
+
+  Future<void> loadUpdates() async {
+    isLoadingUpdates = true;
+    notifyListeners();
+
+    updates = await repository.getLatestUpdates();
+
+    isLoadingUpdates = false;
+    notifyListeners();
+  }
 }
