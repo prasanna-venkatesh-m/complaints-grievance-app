48a0464af8df471e3a89dec8e487d2d91c1b2c6c
Prasanna <prasannavenkatesh.m@arisecapital.in>
2026-08-05 11:59:39 +0530

    Feat : Latest update model added in Home model file

--- lib/features/home/home_model.dart
+++ lib/features/home/home_model.dart
@@ -5,14 +5,47 @@ class DashboardStat {
   DashboardStat({required this.title, required this.value});
 }
 
-class HomeUpdate {
+// =======================
+// LATEST UPDATES
+// =======================
+
+class LatestUpdate {
+  final String id;
   final String category;
   final String title;
-  final String subtitle;
+  final String shortDescription;
+  final String fullContent;
+  final List<String> images;
+  final List<String> videos;
+  final DateTime postedOn;
+  final List<String> links;
+  final String shareUrl;
 
-  HomeUpdate({
+  const LatestUpdate({
+    required this.id,
     required this.category,
     required this.title,
-    required this.subtitle,
+    required this.shortDescription,
+    required this.fullContent,
+    required this.images,
+    required this.videos,
+    required this.postedOn,
+    required this.links,
+    required this.shareUrl,
   });
+
+  factory LatestUpdate.fromJson(Map<String, dynamic> json) {
+    return LatestUpdate(
+      id: json["id"],
+      category: json["category"],
+      title: json["title"],
+      shortDescription: json["shortDescription"],
+      fullContent: json["fullContent"],
+      images: List<String>.from(json["images"]),
+      videos: List<String>.from(json["videos"]),
+      postedOn: DateTime.parse(json["postedOn"]),
+      links: List<String>.from(json["links"]),
+      shareUrl: json["shareUrl"],
+    );
+  }
 }
