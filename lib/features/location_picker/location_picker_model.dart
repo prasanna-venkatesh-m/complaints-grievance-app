d254cf557a3edff2c36521b15666f1e2a23876bf
Prasanna <prasannavenkatesh.m@arisecapital.in>
2026-08-10 21:10:54 +0530

    Location picker model added

---
+++ lib/features/location_picker/location_picker_model.dart
@@ -0,0 +1,11 @@
+class LocationPickerModel {
+  final double latitude;
+  final double longitude;
+  final String address;
+
+  const LocationPickerModel({
+    required this.latitude,
+    required this.longitude,
+    required this.address,
+  });
+}
