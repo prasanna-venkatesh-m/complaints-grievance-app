da8ecc3acf6c56dff0798b0eb4a4a4d946afcbb9
Prasanna <prasannavenkatesh.m@arisecapital.in>
2026-08-13 22:09:08 +0530

    State provider added

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,27 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../l10n/app_localizations.dart';
+import 'providers.dart';
 import 'router/app_router.dart';
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final appLanguage = ref.watch(languageProvider);
+
     SystemChrome.setSystemUIOverlayStyle(
       const SystemUiOverlayStyle(
-        statusBarColor: const Color(0xFFa91145),
-        statusBarIconBrightness: Brightness.light, 
-        statusBarBrightness: Brightness.dark,       
+        statusBarColor: Color(0xFFa91145),
+        statusBarIconBrightness: Brightness.light,
+        statusBarBrightness: Brightness.dark,
       ),
     );
 
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
+
       title: 'TVK Grievance',
+
+      // Global application language.
+      locale: appLanguage.locale,
+
+      // Flutter generated localization configuration.
+      localizationsDelegates:
+          AppLocalizations.localizationsDelegates,
+
+      supportedLocales:
+          AppLocalizations.supportedLocales,
+
       theme: ThemeData(
         fontFamily: 'Poppins',
       ),
+
       routerConfig: appRouter,
+
       builder: (context, child) {
         return Container(
           color: const Color(0xFFa91145),
