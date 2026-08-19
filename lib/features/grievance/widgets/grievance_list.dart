290d239ae0df9bed6534e3ca61b1539120afdc06
Prasanna <prasannavenkatesh.m@arisecapital.in>
2026-08-19 13:41:29 +0530

    Feat : Grievance list translation added

--- lib/features/grievance/widgets/grievance_list.dart
+++ lib/features/grievance/widgets/grievance_list.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tvk_grievance/app/widgets/header_text_widget.dart';
 import 'package:tvk_grievance/features/grievance/widgets/history_grievance_card.dart';
 import 'package:tvk_grievance/features/grievance/widgets/open_grievance_card.dart';
+import 'package:tvk_grievance/l10n/app_localizations.dart';
 
 import '../grievance_model.dart';
 
@@ -12,6 +13,7 @@ class GrievanceList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context)!;
     final openList = grievances.where((e) => e.status != "Resolved").toList();
 
     final history = grievances.where((e) => e.status == "Resolved").toList();
@@ -22,11 +24,11 @@ class GrievanceList extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            HeaderTextWidget(text: "OPEN"),
+            HeaderTextWidget(text: l10n.open),
             const SizedBox(height: 10),
             ...openList.map((e) => OpenGrievanceCard(grievance: e)),
             const SizedBox(height: 20),
-            HeaderTextWidget(text: "HISTORY"),
+            HeaderTextWidget(text: l10n.history),
             ...history.map((e) => HistoryGrievanceCard(grievance: e)),
           ],
         ),
