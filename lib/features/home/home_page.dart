45bccb07944983e7c7a2eff1feef7b1e01f947df
Prasanna <prasannavenkatesh.m@arisecapital.in>
2026-08-23 14:18:10 +0530

    Home page changes

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:tvk_grievance/app/main_layout.dart';
+import 'package:tvk_grievance/app/router/app_routes.dart';
 import 'package:tvk_grievance/l10n/app_localizations.dart';
 
 import 'home_providers.dart';
@@ -15,20 +17,13 @@ class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(
-    BuildContext context,
-    WidgetRef ref,
-  ) {
-    final controller = ref.watch(
-      homeControllerProvider,
-    );
+  Widget build(BuildContext context, WidgetRef ref) {
+    final controller = ref.watch(homeControllerProvider);
 
     final l10n = AppLocalizations.of(context)!;
 
     SystemChrome.setSystemUIOverlayStyle(
-      const SystemUiOverlayStyle(
-        statusBarColor: Colors.white,
-      ),
+      const SystemUiOverlayStyle(statusBarColor: Colors.white),
     );
 
     return MainLayout(
@@ -36,9 +31,7 @@ class HomePage extends ConsumerWidget {
         backgroundColor: const Color(0xffF5F5F5),
         body: SafeArea(
           child: SingleChildScrollView(
-            padding: const EdgeInsets.only(
-              bottom: 90,
-            ),
+            padding: const EdgeInsets.only(bottom: 90),
             child: Column(
               children: [
                 const HomeHeader(),
@@ -54,15 +47,16 @@ class HomePage extends ConsumerWidget {
                 const SizedBox(height: 16),
 
                 Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 16,
-                  ),
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
                   child: Row(
                     children: [
                       Expanded(
                         child: QuickActionCard(
                           icon: "📝",
                           title: l10n.raiseGrievance,
+                          onTap: () {
+                            context.push(AppRoutes.grievances);
+                          },
                         ),
                       ),
                       const SizedBox(width: 12),
@@ -70,6 +64,9 @@ class HomePage extends ConsumerWidget {
                         child: QuickActionCard(
                           icon: "📞",
                           title: l10n.deptContacts,
+                          onTap: () {
+                            context.push(AppRoutes.helpDesk);
+                          },
                         ),
                       ),
                     ],
