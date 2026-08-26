d36db2198c6b7b154410565bce450fd84cf02f80
Prasanna <prasannavenkatesh.m@arisecapital.in>
2026-08-26 19:58:50 +0530

    New Router added

--- lib/app/router/app_routes.dart
+++ lib/app/router/app_routes.dart
@@ -7,4 +7,5 @@ class AppRoutes {
   static const helpDesk = '/help-desk';
   static const grievances = '/grievances';
   static const String contentDetails = '/content-details';
+  static const String grievanceDetails = '/grievance-details';
 }
