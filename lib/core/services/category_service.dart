ff303652581aad9a06a0eec26a0afa7e342ed7f1
prasanna-venkatesh-m <115411975+prasanna-venkatesh-m@users.noreply.github.com>
2026-09-13 11:58:51 +0530

    Update category_service.dart

--- lib/core/services/category_service.dart
+++ lib/core/services/category_service.dart
@@ -1,1 +1,23 @@
 
+import 'package:flutter/material.dart';
+import 'package:tvk_grievance/l10n/app_localizations.dart';
+
+String getCategoryName(
+  BuildContext context,
+  String category,
+) {
+  final l10n = AppLocalizations.of(context)!;
+
+  return switch (category.trim()) {
+    'Water' => l10n.water,
+    'Roads' => l10n.roads,
+    'Electricity' => l10n.electricity,
+    'Drainage' => l10n.drainage,
+    'Garbage' => l10n.garbage,
+    'Public Services' => l10n.publicServices,
+    'Housing & Welfare' => l10n.housingWelfare,
+    'Education & Healthcare' => l10n.educationHealthcare,
+    'Other' => l10n.other,
+    _ => category,
+  };
+}
